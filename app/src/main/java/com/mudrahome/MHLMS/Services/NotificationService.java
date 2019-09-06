d67e888e688694a0b7a42472ab597092475fe113
himanshu <himanshusehgal17@gmail.com>
2019-09-06 10:20:31 +0530

    minor changes

--- app/src/main/java/com/mudrahome/MHLMS/Services/NotificationService.java
+++ app/src/main/java/com/mudrahome/MHLMS/Services/NotificationService.java
@@ -1,11 +1,16 @@
 package com.mudrahome.MHLMS.Services;
 
 import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+
+import android.graphics.Color;
+import android.os.Build;
 import android.util.Log;
 
 import com.mudrahome.MHLMS.R;
@@ -22,11 +27,28 @@ public class NotificationService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        Log.i("NotificationMsg","sgdg");
+        /*Log.i("NotificationMsg","sgdg" + remoteMessage.getData());*/
+
+
         showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
     }
 
     public void showNotification(String title, String message) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(getResources().getString(R.string.notification_channel_id),"User Notification", NotificationManager.IMPORTANCE_HIGH);
+            channel.enableVibration(true);
+            channel.enableLights(true);
+            channel.setLightColor(Color.BLUE);
+            channel.setShowBadge(true);
+            channel.setImportance(NotificationManager.IMPORTANCE_HIGH);
+            channel.setVibrationPattern(new long[]{100,1000,100,1000});
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            assert manager != null;
+            manager.createNotificationChannel(channel);
+        }
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "notification")
                 .setContentTitle(title)
                 .setSmallIcon(R.drawable.ic_stat_name)
