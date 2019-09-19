2ca66e222afd3c93e25b51a28b2ab43fdd01f316
nakulmehra677 <nakulmehra677@gmail.com>
2019-09-19 10:57:25 +0530

    kgtkgjvk

--- app/src/main/java/com/mudrahome/mhlms/Services/NotificationService.java
+++ app/src/main/java/com/mudrahome/mhlms/Services/NotificationService.java
@@ -1,4 +1,4 @@
-package com.mudrahome.mhlms.Services;
+package com.mudrahome.MHLMS.Services;
 
 import android.app.AlarmManager;
 import android.app.NotificationChannel;
@@ -7,14 +7,16 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import android.graphics.Color;
 import android.os.Build;
+import android.util.Log;
 
-import com.mudrahome.mhlms.Activities.LeadListActivity;
-import com.mudrahome.mhlms.R;
+import com.mudrahome.MHLMS.Activities.LeadListActivity;
+import com.mudrahome.MHLMS.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -30,39 +32,46 @@ public class NotificationService extends FirebaseMessagingService {
     Intent resultIntent;
 
 
+
     @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+       showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+
     }
+
 
 
     public void showNotification(String title, String message) {
 
-
         String[] bodymessage = message.split("@@");
 
-        try {
+        Log.d("NotificationService", "showNotification: reviced");
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+
+            NotificationChannel channel=new NotificationChannel(getResources().getString(R.string.notification_channel_id),"User Notification", NotificationManager.IMPORTANCE_HIGH);
+            channel.enableLights(true);
+            channel.setLightColor(Color.BLUE);
+            channel.enableVibration(true);
+            channel.setShowBadge(true);
+            channel.setImportance(NotificationManager.IMPORTANCE_HIGH);
+            channel.setVibrationPattern(new long[]{100, 1000, 100, 1000, 100});
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            assert manager != null;
+            manager.createNotificationChannel(channel);
+
+            Log.d("NotificationService", "showNotification: channel created");
+        }
+        if(!bodymessage[1].isEmpty()){
+
             resultIntent = new Intent(NotificationService.this, LeadListActivity.class)
-                    .putExtra("UIDNotification", bodymessage[1]);
+                    .putExtra("UIDNotification",bodymessage[1]);
+
+            Log.d("UIDNotification", bodymessage[1] +"      "+ bodymessage[0]);
 
             pendingIntent = PendingIntent.getActivity(NotificationService.this, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-
-                NotificationChannel channel = new NotificationChannel("com.mudrahome.MHLMS", "Notification", NotificationManager.IMPORTANCE_HIGH);
-                channel.enableLights(true);
-                channel.setLightColor(Color.BLUE);
-                channel.enableVibration(true);
-                channel.setShowBadge(true);
-                channel.setImportance(NotificationManager.IMPORTANCE_HIGH);
-                channel.setVibrationPattern(new long[]{100, 1000, 100, 1000, 100});
-
-                NotificationManager manager = getSystemService(NotificationManager.class);
-                assert manager != null;
-                manager.createNotificationChannel(channel);
-            }
-
 
             NotificationCompat.Builder builder = new NotificationCompat.Builder(NotificationService.this, getResources().getString(R.string.notification_channel_id))
                     .setContentTitle(title)
@@ -71,7 +80,7 @@ public class NotificationService extends FirebaseMessagingService {
                     .setPriority(NotificationCompat.PRIORITY_HIGH)
                     .setVibrate(new long[]{100, 1000, 100, 1000})
                     .setDefaults(DEFAULT_SOUND | DEFAULT_VIBRATE)
-                    .setStyle(new NotificationCompat.BigTextStyle().bigText(message.trim()))
+                    .setStyle(new NotificationCompat.BigTextStyle().bigText(bodymessage[0].trim()))
                     .setContentText(bodymessage[0])
                     .setContentIntent(pendingIntent);
 
@@ -79,13 +88,16 @@ public class NotificationService extends FirebaseMessagingService {
             managerCompat.notify(id, builder.build());
             id++;
 
-            //if(title.equals("New Lead")){
-            // startTimer();
-            ///}
-
-        } catch (Exception e) {
+            Log.d("NotificationService", "showNotification: notification show");
 
         }
+
+
+        Log.d("AlertReceiver", "showNotification: ");
+
+        //if(title.equals("New Lead")){
+        // startTimer();
+        ///}
     }
 
     private void startTimer() {
@@ -98,8 +110,6 @@ public class NotificationService extends FirebaseMessagingService {
         Intent intent = new Intent(this, AlertReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-        }
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
     }
 }
